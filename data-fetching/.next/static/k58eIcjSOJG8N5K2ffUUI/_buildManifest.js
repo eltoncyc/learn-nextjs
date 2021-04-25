e8117c29ce581ce2a0df7d701f24f9458b3406eb
eltoncyc <mreltonchan@gmail.com>
2021-04-24 17:12:30 -0700

    page pre-rendering & data fetching part 2

--- data-fetching/.next/static/k58eIcjSOJG8N5K2ffUUI/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-fb007e71433e2e2528ad.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-cecafdbaafd2fdd556ba.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
